a008b22df90cb5c361830d8a888f9574e0a7207e
Alexandra Bahdanava <bahdanavaalexandra@gmail.com>
2024-05-20 16:31:36 +0300

    add program

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace MaxAndMin
+{
+    public class Program
+    {
+        public static double Calculate(double firstNumber, double secondNumber, char operation)
+        {
+            double result = 0;
+            switch (operation)
+            {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    break;
+                case '/':
+                    if (secondNumber == 0)
+                    {
+                        Console.WriteLine("Ошибка! Деление на ноль невозможно.");
+                        return double.NaN;
+                    }
+                    result = firstNumber / secondNumber;
+                    break;
+                default:
+                    Console.WriteLine("Ошибка! Некорректная операция.");
+                    return double.NaN;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            double firstNumber, secondNumber;
+            char operation;
+
+            Console.WriteLine("Введите первое число:");
+            while (!double.TryParse(Console.ReadLine(), out firstNumber))
+            {
+                Console.WriteLine("Ошибка! Пожалуйста, введите корректное число:");
+            }
+
+            Console.WriteLine("Введите второе число:");
+            while (!double.TryParse(Console.ReadLine(), out secondNumber))
+            {
+                Console.WriteLine("Ошибка! Пожалуйста, введите корректное число:");
+            }
+
+            Console.WriteLine("Выберите операцию (+, -, *, /):");
+            while (!char.TryParse(Console.ReadLine(), out operation) || (operation != '+' && operation != '-' && operation != '*' && operation != '/'))
+            {
+                Console.WriteLine("Ошибка! Пожалуйста, выберите одну из доступных операций (+, -, *, /):");
+            }
+            Console.WriteLine(Calculate(firstNumber, secondNumber, operation));
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ MaxAndMinTests/ProgramTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MaxAndMin;
+
+namespace MaxAndMin.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void TestMax()
+        {
+            // Arrange
+            double a = 10.0;
+            double b = 5.0;
+            double c = 7.0;
+            double expectedMax = 10.0;
+
+            // Act
+            double actualMax = Program.Max(a, b, c);
+
+            // Assert
+            Assert.AreEqual(expectedMax, actualMax);
+        }
+
+        [TestMethod]
+        public void TestMin()
+        {
+            // Arrange
+            double a = 10.0;
+            double b = 5.0;
+            double c = 7.0;
+            double expectedMin = 5.0;
+
+            // Act
+            double actualMin = Program.Min(a, b, c);
+
+            // Assert
+            Assert.AreEqual(expectedMin, actualMin);
+        }
+    }
+}
